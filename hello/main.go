990fd5e840b84afc262d991e7111d094f2d40334
friendtu <friendtu@gmail.com>
2021-11-02 22:23:01 +0800

    try untype constant

--- hello/main.go
+++ hello/main.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 
 	"example.com/greetings"
+)
+
+func needInt(x int) int {
+	return x * 10
+}
+
+func needFloat(x float64) float64 {
+	return x * 10
+}
+
+const (
+	Big   = 1 << 16
+	Small = Big >> 15
 )
 
 func main() {
@@ -15,4 +29,15 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(messages)
+
+	a := 2
+	b := 3
+	result := math.Sqrt(float64(a*2 + b*2))
+	fmt.Printf("result is %v", result)
+
+	fmt.Printf("needInt(Small): %v\n", needInt(Small))
+	fmt.Printf("needInt(Big): %v\n", needInt(Big))
+	fmt.Printf("needFloat(Small): %v\n", needFloat(Small))
+	fmt.Printf("needFloat(Big): %v\n", needFloat(Big))
+
 }
